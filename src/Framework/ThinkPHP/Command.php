fb41eb2f96323d353c0e11ee94053c1d42fa8924
david <367013672@qq.com>
2024-10-16 14:23:56 +0800

    更新：命令标识

--- src/Framework/ThinkPHP/Command.php
+++ src/Framework/ThinkPHP/Command.php
@@ -20,7 +20,7 @@ class Command extends \think\console\Command
      */
     protected function configure()
     {
-        $this->setName('make:trait')
+        $this->setName('make:db:trait')
             ->addArgument('connection', Argument::REQUIRED, "数据库连接名称")
             ->addArgument('table', Argument::OPTIONAL, "完整的数据表名称")
             ->addOption('all', null, Option::VALUE_NONE, '批量生成所有数据表的注释')
